8dd26bd3cd0a374aabb7944b1a0a0c0ab3eb4e99
olcaneristi <ogulcaneristi123@gmail.com>
2020-10-04 13:59:46 +0300

    loading gecikmesi azaltildi

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1800)
+    setTimeout(() => setLoading(false), 1000)
   }, [])
 
   return (
